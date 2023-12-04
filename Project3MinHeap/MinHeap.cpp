6296814bb34944801d324402f8d4f13fc98e21b4
Christian Cooper <144912285+chrismcpr@users.noreply.github.com>
2023-12-04 15:53:57 -0500

    Added more comments

--- Project3MinHeap/MinHeap.cpp
+++ Project3MinHeap/MinHeap.cpp
@@ -3,18 +3,20 @@
 // Constructor
 MinHeap::MinHeap() {
 
+    // heap is initialized as having 20000 elements
     heap = new VacationData[20000];
     numElements = 0;
 
 }
 
 // insert element into min heap
-void MinHeap::Insert(VacationData k) {
+void MinHeap::Insert(VacationData element) {
 
-    CalculateDifference(k);
+    // sets difference for element
+    CalculateDifference(element);
     int i = numElements;
     numElements++;
-    heap[i] = k;
+    heap[i] = element;
 
     // If parent is greater than child, swap elements
     while(i != 0 && heap[(i - 1) / 2].difference > heap[i].difference) {
@@ -24,9 +26,12 @@ void MinHeap::Insert(VacationData k) {
 
 }
 
+// swaps parent and child if parent > child
 void MinHeap::Swap(VacationData parent, VacationData child, int index) {
 
+    // (index - 1)/2 is the index of the parent, this sets that index to the child element
     heap[(index - 1) / 2] = child;
+    // the child index becomes the parent
     heap[index] = parent;
 
 }
@@ -34,32 +39,37 @@ void MinHeap::Swap(VacationData parent, VacationData child, int index) {
 // calculate the differences between the users budget and the element inserted
 void MinHeap::CalculateDifference(VacationData& element) {
 
-    if (element.month != d.month) {
+    // if the user input month and the data element month are different, 1 gets added to the difference
+    if (element.month != userData.month) {
         element.difference += 1;
     }
 
-    element.difference += abs(d.duration - element.duration);
-    element.difference += abs(d.age - element.age);
+    // the difference from the user input for age and duration are added to difference for that element
+    element.difference += abs(userData.duration - element.duration);
+    element.difference += abs(userData.age - element.age);
 
-    if (element.gender != d.gender) {
+    // if the user input gender and accomodation and the data element gender and accomodation are different, 1 gets added to the difference
+    if (element.gender != userData.gender) {
         element.difference += 1;
     }
-    if (element.accommodation != d.accommodation) {
+    if (element.accommodation != userData.accommodation) {
         element.difference += 1;
     }
 
-    element.difference += abs(d.budget - element.budget);
+    // if the budgets are different, the difference between the budgets are added to the difference
+    element.difference += abs(userData.budget - element.budget);
 
 }
 
+// sets parameters based on user input
 void MinHeap::SetParameters(string _month, int _duration, int _age, string _gender, string _accomodation, double _budget) {
 
-    d.month = _month;
-    d.duration = _duration;
-    d.age = _age;
-    d.gender = _gender;
-    d.accommodation = _accomodation;
-    d.budget = _budget;
+    userData.month = _month;
+    userData.duration = _duration;
+    userData.age = _age;
+    userData.gender = _gender;
+    userData.accommodation = _accomodation;
+    userData.budget = _budget;
 
 }
 
--- Project3MinHeap/MinHeap.h
+++ Project3MinHeap/MinHeap.h
@@ -6,15 +6,16 @@ class MinHeap {
 
 private:
     VacationData *heap; // MinHeap Array
-    VacationData d;
+    VacationData userData;
     int numElements;
 
 
 public:
     MinHeap();
-    void Insert(VacationData k);
-    void Swap(VacationData parent, VacationData child, int index);
-    void CalculateDifference(VacationData& elements);
+    void Insert(VacationData element); // insert value into heap
+    void Swap(VacationData parent, VacationData child, int index); // swap values in heap
+    void CalculateDifference(VacationData& elements); // calculate difference based on user input
+    //initialize user input
     void SetParameters(string _month, int _duration, int _age, string _gender, string _accomodation, double _budget);
-    VacationData Peek();
+    VacationData Peek(); // returns heap[0]
 };
